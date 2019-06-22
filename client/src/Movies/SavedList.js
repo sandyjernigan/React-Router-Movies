b04733f7417649121d617bb5a16d4a45006af717
sandyjernigan <sandy.jernigan@gmail.com>
2019-06-22 13:15:40 -0400

    Turned Links into NavLinks with active class saved-active

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -12,9 +12,9 @@ export default class SavedList extends Component {
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
           <span className="saved-movie">
-            <Link to={`/movies/${movie.id}`} key={movie.id}>
+            <NavLink to={`/movies/${movie.id}`} activeClassName="saved-active">
               {movie.title}
-            </Link>
+            </NavLink>
           </span>
         ))}
         <Link to="/">
