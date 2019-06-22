ac2e4e9cea69f48bfbd50d4ce74fff6d3dc797e1
sandyjernigan <sandy.jernigan@gmail.com>
2019-06-22 13:02:59 -0400

    Refactor so that our code is DRY.
    new component in MovieCard.js that returns the movie card data so it can be reused

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import axios from 'axios';
+import MovieCard from './MovieCard';
 
 export default class Movie extends Component {
   constructor(props) {
@@ -43,26 +44,11 @@ export default class Movie extends Component {
       return <div>Loading movie information...</div>;
     }
 
-    const { title, director, metascore, stars } = this.state.movie;
+    // const { title, director, metascore, stars } = this.state.movie;
 
     return (
       <div className="save-wrapper">
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div>
+        <MovieCard movie={this.state.movie} />
         <Link to="/">
           <div className="save-button" onClick={this.saveMovie}>Save</div>
         </Link>
